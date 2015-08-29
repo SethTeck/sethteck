491534c6c402d4757800d5fb469906339cbdb08d
Seth Allen <seth.a@me.com>
2015-08-29 00:36:45 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sethteck::Application.config.secret_key_base = 'b94eed8265fa506c6cf70fcabdf687393c7cd8b020588e6e3bf08df55a5acb19397f13a298f1299e64322f567efc6391eccaf71dbaec120b73c9ed522b6045a3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sethteck::Application.config.session_store :cookie_store, key: '_sethteck_session'

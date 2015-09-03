088381f2118fea9521db7aa77b6f2c430e2f18e4
Seth Allen <seth.a@me.com>
2015-09-03 03:00:13 +0000

    created and migrated database

---
+++ db/migrate/20150903024749_create_contacts.rb
@@ -0,0 +1,13 @@
+class CreateContacts < ActiveRecord::Migration
+  def change
+    create_table :contacts do |t|
+
+    	t.string :firstname
+    	t.string :lastname
+    	t.string :email
+    	t.text :comment
+
+      t.timestamps
+    end
+  end
+end
